b02ea02d6079bdf6403fd6b0e057fb0c4953eebf
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-04-17 14:51:28 -0300

    test: teste de sucesso e erro da service

--- src/test/java/com/template/services/MeteorologicalDataTests.java
+++ src/test/java/com/template/services/MeteorologicalDataTests.java
@@ -5,7 +5,6 @@ import com.template.data.dto.MeteorologicalDataDTO;
 import com.template.data.entity.MeteorologicalDataEntity;
 import com.template.data.enums.WeatherEnum;
 import com.template.data.repository.MeteorologicalDataRepository;
-import com.template.presentation.controller.MeteorologicalDataController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,11 +14,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.util.Optional;
-import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -33,42 +32,39 @@ public class MeteorologicalDataTests {
     @InjectMocks
     private MeteorologicalDataService meteorologicalDataService;
 
-
-    MeteorologicalDataEntity meteorologicalData = MeteorologicalDataEntity.builder()
-            .id(5)
-            .city("Porto Alegre")
-            .weatherDate(LocalDate.of(2023, 4, 12))
-            .maxTemperature(32)
-            .minTemperature(19)
-            .morningWeather(WeatherEnum.SUN)
-            .nightWeather(WeatherEnum.CLOUDY)
-            .humidity(12)
-            .precipitation(10)
-            .windSpeed(11)
-            .build();
-
-    MeteorologicalDataEntity meteorologicalDataSemID = MeteorologicalDataEntity.builder()
-            .city("Porto Alegre")
-            .weatherDate(LocalDate.of(2023, 4, 12))
-            .maxTemperature(32)
-            .minTemperature(19)
-            .morningWeather(WeatherEnum.SUN)
-            .nightWeather(WeatherEnum.CLOUDY)
-            .humidity(12)
-            .precipitation(10)
-            .windSpeed(11)
-            .build();
-
     MeteorologicalDataDTO meteorologicalDataDTO = new MeteorologicalDataDTO("Porto Alegre",
+            LocalDate.of(2023, 4, 12),
+            WeatherEnum.SUN, WeatherEnum.CLOUDY, 32, 19, 12, 10, 11);
+
+    MeteorologicalDataEntity meteorologicalData = new MeteorologicalDataEntity("Porto Alegre",
+            LocalDate.of(2023, 4, 12),
+            WeatherEnum.SUN, WeatherEnum.CLOUDY, 32, 19, 12, 10, 11);
+
+    MeteorologicalDataDTO meteorologicalDataDTOInvalid = new MeteorologicalDataDTO(null,
+            LocalDate.of(2023, 4, 12),
+            WeatherEnum.SUN, WeatherEnum.CLOUDY, 32, 19, 12, 10, 11);
+
+    MeteorologicalDataEntity meteorologicalDataInvalid = new MeteorologicalDataEntity(null,
             LocalDate.of(2023, 4, 12),
             WeatherEnum.SUN, WeatherEnum.CLOUDY, 32, 19, 12, 10, 11);
 
     @Test
     @DisplayName("POST - Success")
     void createSucess() {
+        when(meteorologicalDataRepository.save(any())).thenReturn(meteorologicalData);
+
         MeteorologicalDataEntity newMeteorologicalData = meteorologicalDataService.create(meteorologicalDataDTO);
         assertNotNull(newMeteorologicalData);
-        System.out.println(meteorologicalDataDTO.city());
-        System.out.println("DATA: " + newMeteorologicalData);
+        verify(meteorologicalDataRepository).save(any());
+    }
+
+    @Test
+    @DisplayName("POST - Error")
+    void createError() {
+        when(meteorologicalDataRepository.save(any())).thenReturn(meteorologicalDataInvalid);
+
+        MeteorologicalDataEntity newMeteorologicalData = meteorologicalDataService.create(meteorologicalDataDTOInvalid);
+        assertNull(newMeteorologicalData.getCity());
+        verify(meteorologicalDataRepository).save(any());
     }
 }
