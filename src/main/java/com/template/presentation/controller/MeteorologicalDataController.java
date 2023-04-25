4203eb69a4e911d9b905c8e6d5fe065793882969
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-04-25 10:12:10 -0300

    :fire: removendo imports não utilizados

--- src/main/java/com/template/presentation/controller/MeteorologicalDataController.java
+++ src/main/java/com/template/presentation/controller/MeteorologicalDataController.java
@@ -12,9 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
 @CrossOrigin(origins = "http://localhost:4767")
 @RestController
 @RequestMapping("/meteorologicalData")
